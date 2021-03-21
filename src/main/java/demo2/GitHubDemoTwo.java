c8223ac51d2bc58f4ba28581c27cec2b2a0be1c3
root <root@Khuno>
2021-03-21 15:26:09 -0400

    second change

--- src/main/java/demo2/GitHubDemoTwo.java
+++ src/main/java/demo2/GitHubDemoTwo.java
@@ -1,5 +1,8 @@
 package demo2;
 
 public class GitHubDemoTwo {
-
+public static void main(String[] args) {
+	System.out.println("My First GitHub Commint..............");
+	System.out.println("My second change  GitHub Commint..............");
+}
 }
